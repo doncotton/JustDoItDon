040344cb55c2fa703a1ad01ba2ec150826a4f297
Prodkt <technology@prodkt.cloud>
2024-04-02 16:35:22 -0600

    hybrid to static - avoid serverless functions interim

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel(),
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },}),
   integrations: [tailwind({
     nesting: true,}), react()]
 });
