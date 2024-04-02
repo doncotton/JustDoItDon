447c4bbb40658cf4defdc9c8dd65be48e04d96bc
Prodkt <technology@prodkt.cloud>
2024-04-02 16:55:03 -0600

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
